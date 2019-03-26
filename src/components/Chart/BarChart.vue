26180ec3898ed1dc56b1f80470dc2b02d36465e0
draconian00 <draconian00@gmail.com>
2019-03-26 15:16:52 +0900

    bar-chart 추가

---
+++ src/components/Chart/BarChart.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Component, Prop } from 'vue-property-decorator';
+import BaseChart from './BaseChart.vue';
+import { ChartType } from 'chart.js';
+
+@Component({
+  name: 'bar-chart',
+})
+export default class BarChart extends BaseChart {
+  @Prop() public width!: number;
+  @Prop() public height!: number;
+
+  public chartId: string = 'bar-chart';
+  public chartType: ChartType = 'bar';
+  public styles: object = {
+    width: this.width ? `${this.width}px` : 'auto',
+    height: this.width ? `${this.height}px` : 'auto',
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,41 +1,72 @@
 <template>
   <div class="test-view">
     <swiper ref="testSwiper" >
-      <swiper-slide>
-        <p>1</p>
+      <swiper-slide style="background-color:#0070F0; color:#FFF">
+        <p>이번 달 지출이 많으시네요.<br>지난달 보다 857,400원 더 쓰셨어요.</p>
+        <line-chart :chartdata="testData" :options="testOptions" />
       </swiper-slide>
       <swiper-slide>
-        <p>2</p>
+        <p>이번 달 지출이 많으시네요.<br>지난달 보다 857,400원 더 쓰셨어요.</p>
+        <bar-chart :chartdata="testData" :options="testOptions" />
       </swiper-slide>
     </swiper>
-    <line-chart :chartdata="testData" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import LineChart from '@/components/Chart/LineChart.vue';
-import { ChartData } from 'chart.js';
+import BarChart from '@/components/Chart/BarChart.vue';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   name: 'TestView',
   components: {
     LineChart,
+    BarChart,
   },
 })
 export default class TestView extends Vue {
 
   private testData: ChartData = {
-    labels: ['6월', '7월', '8월', '9월', '10월'],   // Array<string | string[]>;
-    datasets: [
-      {
-        showLine: true,
-        fill: false,
-        label: 'label',
-        data: [1200000, 1500000, 1300000, 1400000, 1700000],
-        lineTension: 0,
-      },
-    ],
+      labels: ['6월', '7월', '8월', '9월', '10월', '11월'],
+      datasets: [{
+          // label: '# of Votes',
+          data: [3, 4, 2, 3, 3, 11],
+          fill: false,
+          // backgroundColor: 'rgba(255, 255, 255, 0.1)',
+          borderColor: 'rgba(255, 255, 255, 1)',
+          pointBackgroundColor: 'rgba(255, 255, 255, 0.4)',
+          borderWidth: 5,
+          lineTension: 0.1,
+          pointRadius: 0,
+      }]
+  };
+  private testOptions: ChartOptions = {
+    events: ['click'],
+    onClick: (e) => {
+      console.log('line-chart click', e);
+    },
+    legend: {
+      display: false,
+    },
+    scales: {
+      yAxes: [{
+        display: false,
+        ticks: {
+          beginAtZero: true,
+        },
+      }],
+      xAxes: [{
+        ticks: {
+          fontColor: '#FFF',
+        },
+        gridLines: {
+          borderDash: [4],
+          color: 'rgba(255, 255, 255, 0.4)',
+        },
+      }],
+    },
   };
 }
 </script>
